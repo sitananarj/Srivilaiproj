c2e24ac4e4413feaf0478414afea3d7b4ef5fc5c
sitanan wankhiew <wankhiew_s@silpakorn.edu>
2020-01-18 22:32:35 +0700

    storyboard01

---
+++ Controller/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Srivilaiproj
+//
+//  Created by siwa on 16/1/2563 BE.
+//  Copyright © 2563 siwa. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
